a915cb7fa080f4875ec3cdd372a14403f8ef5412
JeanCSinisterra <84473537+JeanCSinisterra@users.noreply.github.com>
2023-08-01 22:23:18 -0500

    styles & html coded also part of the JS

---
+++ index.js
@@ -0,0 +1,28 @@
+// Build the functions of add, subtract, multiply, and divide using the arithmetic operators.
+function add(a, b) {
+    return a + b;
+}
+function subtract(a, b) {
+    return a - b;
+}
+function multiply(a, b) {
+    return a * b;
+}
+function divide(a, b) {
+    return a / b;
+}
+
+
+// Create a function called Operate that takes an operator and 2 numbers and then calls one of the above functions on the Numbers
+function operate(operator, a, b) {
+    if (operator === '+') {
+        return add(a, b);
+    } else if (operator === '-') {
+        return subtract(a, b);
+    } else if (operator === '*') {
+        return multiply(a, b);
+    } else if (operator === '/') {
+        return divide(a, b);
+    }
+}
+
